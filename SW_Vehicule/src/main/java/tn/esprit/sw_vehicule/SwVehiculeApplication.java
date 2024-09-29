20438d1f87f88d5afb0de4f8091d9aebc20a1d9a
MohamedYassineZaidi <123814527+MohamedYassineZaidi@users.noreply.github.com>
2024-09-29 16:48:59 +0200

    CRUD

---
+++ SW_Vehicule/src/main/java/tn/esprit/sw_vehicule/SwVehiculeApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.sw_vehicule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwVehiculeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwVehiculeApplication.class, args);
+    }
+
+}
---
+++ SW_Vehicule/src/test/java/tn/esprit/sw_vehicule/SwVehiculeApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.sw_vehicule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwVehiculeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
