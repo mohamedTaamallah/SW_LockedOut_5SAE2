ada2ab82f26549b3ae01e81e862898d36eb075ad
Taamallah Mohamed <93285499+mohamedTaamallah@users.noreply.github.com>
2024-10-06 11:10:24 +0200

    Update : config
    Updated the config related to the eureka server

--- src/test/java/lockedout/sae/vehiculemicroservice/VehiculeMicroServiceApplicationTests.java
+++ src/test/java/lockedout/sae/vehiculemicroservice/VehiculeMicroServiceApplicationTests.java
@@ -2,7 +2,8 @@ package lockedout.sae.vehiculemicroservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@EnableDiscoveryClient
 @SpringBootTest
 class VehiculeMicroServiceApplicationTests {
 
